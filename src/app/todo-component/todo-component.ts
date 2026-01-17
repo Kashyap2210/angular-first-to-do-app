cedec9ff14c95e859600eaea96c1ff3e73c6522d
Kashyap Solanki <kash.cdac@gmail.com>
2026-01-17 20:54:48 +0530

    input-form-implement

--- src/app/todo-component/todo-component.ts
+++ src/app/todo-component/todo-component.ts
@@ -1,10 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { Component, inject } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { TodoService, TodoStatusEnum } from './todo-service';
 
 @Component({
   selector: 'app-todo-component',
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './todo-component.html',
   styleUrls: ['./todo-component.css'],
   standalone: true,
@@ -14,4 +15,21 @@ export class TodoComponent {
   TodoStatusEnum = TodoStatusEnum;
 
   todos = this.todoService.todos;
+
+  // local ui state
+  id: number = 0;
+  title: string = '';
+  description: string = '';
+
+  addTodo() {
+    this.todoService.addTodo({ title: this.title, description: this.description });
+  }
+
+  updateTodo(id: number) {
+    this.todoService.updateTodo(id, { title: this.title, description: this.description });
+  }
+
+  removeTodo(id: number) {
+    this.todoService.removeTodo(id);
+  }
 }
