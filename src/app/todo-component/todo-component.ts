13bbf0fc23a8efd391074ea177bc8ba70e9bb320
Kashyap Solanki <kash.cdac@gmail.com>
2026-01-18 16:24:28 +0530

    updates-now-working

--- src/app/todo-component/todo-component.ts
+++ src/app/todo-component/todo-component.ts
@@ -1,14 +1,17 @@
 import { CommonModule, DatePipe } from '@angular/common';
 import { Component, inject } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { NgIcon, provideIcons } from '@ng-icons/core';
+import { heroCheckCircle, heroClock, heroUsers } from '@ng-icons/heroicons/outline';
 import { TodoService, TodoStatusEnum } from './todo-service';
 
 @Component({
   selector: 'app-todo-component',
-  imports: [CommonModule, FormsModule, DatePipe],
+  imports: [CommonModule, FormsModule, DatePipe, NgIcon],
   templateUrl: './todo-component.html',
   styleUrls: ['./todo-component.css'],
   standalone: true,
+  viewProviders: [provideIcons({ heroUsers, heroCheckCircle, heroClock })],
 })
 export class TodoComponent {
   private readonly todoService = inject(TodoService);
@@ -31,5 +34,14 @@ export class TodoComponent {
 
   removeTodo(id: number) {
     this.todoService.removeTodo(id);
+  }
+
+  markToDoDone(id: number) {
+    console.log(1);
+    this.todoService.updateTodo(id, { status: TodoStatusEnum.DONE });
+  }
+
+  markToDoPostpone(id: number) {
+    this.todoService.updateTodo(id, { status: TodoStatusEnum.POSTPONE });
   }
 }
